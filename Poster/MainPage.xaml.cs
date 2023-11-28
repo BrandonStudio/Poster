c4578ba02cafc0e7448cef7a0a2ef6e99bbc1a4d
BrandonStudio <brandonstudio@outlook.com>
2023-11-28 09:58:23 +0800

    simple logic

--- Poster/MainPage.xaml.cs
+++ Poster/MainPage.xaml.cs
@@ -6,5 +6,50 @@ public partial class MainPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+	private void OnUrlChanged(object sender, TextChangedEventArgs e)
+	{
+		if (string.IsNullOrWhiteSpace(urlInput.Text))
+			urlHintLabel.Text = "URL";
+		else if (!urlInput.Text.StartsWith("http://") && !urlInput.Text.StartsWith("https://"))
+			urlHintLabel.Text = "http://" + urlInput.Text;
+		else
+			urlHintLabel.Text = urlInput.Text;
+	}
+
+	private async void StartSend(object sender, EventArgs e)
+	{
+		responseStatusBox.Text = "Sending...";
+		responseStatusBox.TextColor = Colors.Gray;
+		responseBodyBox.Text = string.Empty;
+		await SendAsync();
+	}
+
+	private async Task SendAsync()
+	{
+		HttpClient client = new()
+		{
+			Timeout = TimeSpan.FromSeconds(120)
+		};
+		HttpContent content = null;
+		if (!string.IsNullOrWhiteSpace(requestBodyInput.Text))
+			content = new StringContent(requestBodyInput.Text, null, mediaType: contentTypeInput.Text);
+		try
+		{
+			var result = await client.PostAsync(urlHintLabel.Text, content);
+			responseStatusBox.Text = result.StatusCode.ToString();
+			if (!result.IsSuccessStatusCode)
+				responseStatusBox.TextColor = Colors.Red;
+			else
+				responseStatusBox.TextColor = Colors.Green;
+			var response = await result.Content.ReadAsStringAsync();
+			responseBodyBox.Text = response;
+		}
+		catch (HttpRequestException ex)
+		{
+			responseStatusBox.Text = ex.Message;
+			responseStatusBox.TextColor = Colors.Red;
+		}
+	}
 }
 
